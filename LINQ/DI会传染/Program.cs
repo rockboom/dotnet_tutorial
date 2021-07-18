374647f65c0db268f9eca6154409833af8df4b1b
rockboom <nicaiwho@163.com>
2021-07-18 10:40:31 +0800

    add LINQ project

---
+++ LINQ/DI会传染/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+namespace DI_Basic_usage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddScoped<Conroller>();
+            services.AddScoped<ILog,LogImpl>();
+            //services.AddScoped<IConfig,ConfigImpl>();
+            services.AddScoped<IConfig,DbConfigImpl>();
+            services.AddScoped<IStorage, StorageImpl>();
+            using (var sp = services.BuildServiceProvider())
+            {
+                var c = sp.GetRequiredService<Conroller>();
+                c.Test();
+            }
+        }
+    }
+    class Conroller
+    {
+        private readonly ILog log;
+        private readonly IStorage storage;
+        public Conroller(ILog log, IStorage storage)
+        {
+            this.log = log;
+            this.storage = storage;
+        }
+        public void Test()
+        {
+            this.log.Log("开始上传");
+            this.storage.Save("11111111111","1.txt");
+            this.log.Log("上传完毕");
+        }
+    }
+    interface ILog
+    {
+        void Log(string msg);
+    }
+    class LogImpl : ILog
+    {
+        public void Log(string msg)
+        {
+            Console.WriteLine($"记录日志:{msg}");
+        }
+    }
+    interface IConfig
+    {
+        string GetValue(string name);
+    }
+    class ConfigImpl : IConfig
+    {
+        public string GetValue(string name)
+        {
+            return "Hello";
+        }
+    }
+    class DbConfigImpl : IConfig
+    {
+        public string GetValue(string name)
+        {
+            Console.WriteLine("从数据库读取配置");
+            return "Hello Db";
+        }
+    }
+    interface IStorage
+    {
+        void Save(string content,string name);
+    }
+    class StorageImpl : IStorage
+    {
+        private readonly IConfig config;
+        public StorageImpl(IConfig config)
+        {
+            this.config = config;
+        }
+        public void Save(string content, string name)
+        {
+            string server = config.GetValue("server");
+            Console.WriteLine($"向服务器{server}的文件名为{name}上传{content}");
+        }
+    }
+}
---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+namespace LINQ
+{
+    class Program
+    {
+        public static  void Main()
+        {
+            int[] nums = new int[] { 3, 4, 5, 67, 8, 234 };
+            //IEnumerable<int> result = nums.Where(a => a > 10);
+            IEnumerable<int> result = MyWhere1(nums,a => a > 10);
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+        }
+        static IEnumerable<int> MyWhere1(IEnumerable<int> nums, Func<int, bool> f)
+        {
+            List<int> al = new List<int>();
+            foreach(int n in nums)
+            {
+                if (f(n)) al.Add(n);
+            }
+            return al;
+        }
+        static IEnumerable<int> MyWhere2(IEnumerable<int> nums, Func<int, bool> f)
+        {
+            foreach (int n in nums)
+            {
+                if (f(n)) yield return n;
+            }
+        }
+
+    }
+    
+}
